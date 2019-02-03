06e00e45292a758ef56458db85c60588f80a3ec4
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2019-02-04 10:06:50 +1100

    Add failing test of passing magicc init args

--- tests/integration/test_adapters.py
+++ tests/integration/test_adapters.py
@@ -1,4 +1,5 @@
 from abc import ABCMeta, abstractmethod
+from unittest.mock import Mock
 
 
 from openscm.adapters import MAGICC6
@@ -16,3 +17,13 @@ class _AdapterTester(object):
 
 class TestMAGICCAdapter(_AdapterTester):
 	tadapter = MAGICC6
+
+	def test_init(self, test_adapter):
+		super().test_init()
+
+	def test_init_arg_passing(self, test_adapter):
+		test_adapter.magicc = Mock()
+		tkwargs = {"here": "there", "everywhere": "nowhere"}
+		test_adapter.initialize(**tkwargs)
+
+		test_adapter.magicc.assert_called_with(**tkwargs)
