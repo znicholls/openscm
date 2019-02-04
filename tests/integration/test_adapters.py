c2cf41550fde0875932726026f53f33c1df961cb
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2019-02-04 14:31:29 +1100

    Add failing test of set_config

--- tests/integration/test_adapters.py
+++ tests/integration/test_adapters.py
@@ -2,7 +2,27 @@ from abc import ABCMeta, abstractmethod
 from unittest.mock import Mock, MagicMock
 
 
+import pytest
+
+
 from openscm.adapters import MAGICC6, Hector
+from openscm.core import ParameterSet
+
+
+@pytest.fixture(scope="function")
+def test_adapter(request):
+    yield request.cls.tadapter()
+
+
+@pytest.fixture(scope="function")
+def test_config_paraset():
+	parameters = ParameterSet()
+	ecs_writable = parameters.get_writable_scalar_view(
+		"ecs",
+		"World",
+		"K"
+	)
+	ecs_writable.set(3)
 
 
 class _AdapterTester(object):
@@ -17,6 +37,13 @@ class _AdapterTester(object):
 	def test_shutdown(self, test_adapter):
 		test_adapter.initialize()
 		test_adapter.shutdown()
+
+	def test_set_config(self, test_adapter, test_config_paraset):
+		with pytest.raises(ModelUninitialisedError):
+			test_adapter.set_config(test_config_paraset)
+
+		test_adapter.initialize()
+		test_adapter.set_config(test_config_paraset)
 
 
 class TestMAGICCAdapter(_AdapterTester):
@@ -42,6 +69,16 @@ class TestMAGICCAdapter(_AdapterTester):
 		test_adapter.magicc = MagicMock()
 		test_adapter.shutdown()
 		test_adapter.magicc.__exit__.assert_called()
+
+	def test_set_config(self, test_adapter, test_config_paraset):
+		super().test_set_config(test_adapter, test_config_paraset)
+
+		test_adapter.initialize()
+		test_adapter.set_config(test_config_paraset)
+
+		magicc_config = test_adapter.magicc.update_config()
+
+		assert magicc_config["core_climatesensitivity"] == 3
 
 
 class TestHectorAdapter(_AdapterTester):
