1339778df5ebc414710e2dedc03144f9b6c1d0c2
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2019-02-06 12:54:45 +1100

    Add constants module

---
+++ openscm/constants.py
@@ -0,0 +1,6 @@
+"""Helpful constants.
+"""
+
+from .units import unit_registry
+
+ONE_YEAR = 1*unit_registry("yr")
---
+++ tests/unit/test_constants.py
@@ -0,0 +1,4 @@
+from openscm.constants import ONE_YEAR
+
+def test_one_year():
+    assert ONE_YEAR.to("s").magnitude == 31556925.9747
