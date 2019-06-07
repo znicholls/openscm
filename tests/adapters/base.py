ff08ca6ca5043e3253001e9a3e824db3f3d2ec9c
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2019-06-07 21:02:33 +1000

    Clean up adapter testing base class

--- tests/adapters/base.py
+++ tests/adapters/base.py
@@ -18,114 +18,61 @@ class _AdapterTester(metaclass=ABCMeta):
     """
     Adapter to test
     """
-
+    @abstractmethod
     def test_initialize(self, test_adapter):
         """
         Test the adapter is initiated as intended.
 
-        Extra tests can be added for different adapters, depending on whether there
-        should be any other behaviour as part of ``__init__`` calls.
+        Extra tests should be added for different adapters, to check any other
+        expected behaviour as part of ``__init__`` calls.
         """
         test_adapter._initialize_model()
 
+    @abstractmethod
     def test_shutdown(self, test_adapter):
         """
         Test the adapter can be shutdown.
 
-        Extra tests can be adapted depending on what the adapter should actually
+        Extra tests should be added depending on what the adapter should actually
         do on shutdown.
         """
         del test_adapter
 
+    @abstractmethod
     def test_initialize_model_input(self, test_adapter):
         """
         Test that initalizing model input does as intended.
 
-        Extra tests can be adapted depending on what the adapter should actually
-        do when initialised.
+        Extra tests should be added depending on what the adapter should actually
+        do when model input is initialised.
         """
         assert not test_adapter._initialized
         test_adapter.initialize_model_input()
         assert test_adapter._initialized
 
-        # TODO: add tests something like below
-        # assert (
-        #     test_adapter._parameters.get_scalar_view(("ecs",), ("World",), "K").get()
-        #     == 3
-        # )
-        # assert (
-        #     test_adapter._parameters.get_scalar_view(
-        #         ("rf2xco2",), ("World",), "W / m^2"
-        #     ).get()
-        #     == 4
-        # )
-
-    # TODO: move this into issue about run tests
-    # def test_initialize_model_input_non_model_parameter(
-    #     self, test_adapter, test_drivers
-    # ):
-    #     tname = ("junk",)
-    #     test_adapter._parameters.get_writable_scalar_view(tname, ("World",), "K").set(4)
-    #     test_adapter.initialize_model_input()
-    #     # TODO test that "junk" has not been used
-
+    @abstractmethod
     def test_initialize_run_parameters(self, test_adapter, test_run_parameters):
         """
         Test that initalizing run parameters does as intended.
+
+        Extra tests should be added depending on what the adapter should actually
+        do when run parameters are initialised.
         """
         self.prepare_run_input(
             test_adapter, test_run_parameters.start_time, test_run_parameters.stop_time
         )
-        # TODO: uncomment these tests and check passing worked as intended
-        # expected = test_drivers["setters"]
-        # in_parameters = test_drivers["ParameterSet"]
-        # start_time = 30
-        # stop_time = 250 * 365 * 24 * 60 * 60
-        # in_parameters.get_writable_scalar_view(
-        #     ("start_time",), ("World",), "s"
-        # ).set(start_time)
-        # in_parameters.get_writable_scalar_view(
-        #     ("stop_time",), ("World",), "s"
-        # ).set(stop_time)
-
-        # out_parameters = ParameterSet()
-        # tadapter = self.tadapter(in_parameters, out_parameters)
-
         assert not test_adapter._initialized
         test_adapter.initialize_run_parameters()
         assert test_adapter._initialized
 
-        # TODO: uncomment these tests and check passing worked as intended
-        # assert tadapter._start_time == start_time
-        # assert tadapter._stop_time == stop_time
-
-        # assert (
-        #     in_parameters.get_scalar_view(("start_time",), ("World",), "s").get()
-        #     == start_time
-        # )
-        # assert (
-        #     in_parameters.get_scalar_view(("stop_time",), ("World",), "s").get()
-        #     == stop_time
-        # )
-        # assert (
-        #     in_parameters.get_scalar_view("ecs", ("World",), "K").get() == expected["ecs"]
-        # )
-        # assert (
-        #     in_parameters.get_scalar_view(
-        #         ("rf2xco2",), ("World",), "W / m^2"
-        #     ).get()
-        #     == expected["rf2xco2"]
-        # )
-        # np.testing.assert_allclose(
-        #     tadapter._parameters.get_timeseries_view(("Emissions", "CO2"), ("World",), "GtCO2/yr", expected["emissions_time_points"], ParameterType.AVERAGE_TIMESERIES).get(),
-        #     expected["emissions"],
-        #     rtol=1e-10,
-        #     atol=1e-15,
-        # )
-
+    @abstractmethod
     def test_run(self, test_adapter, test_run_parameters):
-        # TODO: add some tests about where input and output go to make sure adapter
-        # isn't overwriting anything it's not meant to by accident
+        """
+        Test that running the model does as intended.
+
+        Extra tests should be added depending on what the adapter should actually
+        do when run with the parameters provided by `test_run_parameters`.
+        """
         self.prepare_run_input(
             test_adapter, test_run_parameters.start_time, test_run_parameters.stop_time
         )
@@ -134,9 +81,19 @@ class _AdapterTester(metaclass=ABCMeta):
         test_adapter.reset()
         test_adapter.run()
 
+    @abstractmethod
     def test_step(self, test_adapter, test_run_parameters):
-        # TODO: add some tests about where input and output go to make sure adapter
-        # isn't overwriting anything it's not meant to by accident
+        """
+        Test that stepping the model does as intended.
+
+        Extra tests should be added depending on what the adapter should do when
+        stepped.
+        """
+        # if your model cannot step, override this method with the below
+        """
+        pytest.skip("Step unavailable for {}".format(type(test_adapter)))
+        """
+        # otherwise, use the below as a base
         self.prepare_run_input(
             test_adapter, test_run_parameters.start_time, test_run_parameters.stop_time
         )
@@ -144,22 +101,9 @@ class _AdapterTester(metaclass=ABCMeta):
         test_adapter.initialize_run_parameters()
         test_adapter.reset()
         assert test_adapter._current_time == test_run_parameters.start_time
-        try:
-            new_time = test_adapter.step()
-            assert new_time > test_run_parameters.start_time
-        except NotImplementedError:
-            pytest.skip("Step unavailable for {}".format(type(test_adapter)))
 
-    # TODO: put this sort of test in issue about run function tests
-    # def test_initialize_run_parameters_non_model_parameter(self, test_adapter):
-    #     tname = ("junk",)
-    #     test_adapter._parameters.get_writable_scalar_view(tname, ("World",), "K").set(4)
-    #     error_msg = re.escape(
-    #         "{} is not a {} parameter".format(tname[0], self.tadapter.__name__)
-    #     )
-
-    # with pytest.raises(NotAnScmParameterError, match=error_msg):
-    #     test_adapter.initialize_run_parameters()
+        new_time = test_adapter.step()
+        assert new_time > test_run_parameters.start_time
 
     @abstractmethod
     def test_openscm_standard_parameters_handling(self):
@@ -174,4 +118,45 @@ class _AdapterTester(metaclass=ABCMeta):
 
         We give an example of how such a test might look below.
         """
-        pass  # TODO: implement once parameter usage can be checked
+        parameters = ParameterSet()
+        parameters.generic("Start Time").value = np.datetime64("1850-01-01")
+        parameters.generic("Stop Time").value = np.datetime64("2100-01-01")
+        ecs_magnitude = 3.12
+        parameters.scalar("Equilibrium Climate Sensitivity", "delta_degC").value = ecs_magnitude
+        output_parameters = ParameterSet()
+
+        test_adapter = self.tadapter(parameters, output_parameters)
+
+        self.prepare_run_input(
+            test_adapter,
+            parameters.generic("Start Time").value,
+            parameters.generic("Stop Time").value,
+        )
+        test_adapter.initialize_model_input()
+        test_adapter.initialize_run_parameters()
+        test_adapter.reset()
+        test_adapter.run()
+
+        # From here onwards you can test whether e.g. the parameters have been used as
+        # intended, an error was thrown or the parameters were not used.
+        # If you're testing the parameters are used as intended, it might look
+        # something like:
+        assert (
+            test_adapter._parameters.scalar(
+                ("Model name", "model ecs parameter"), "delta_degC"
+            ).value
+            == ecs_magnitude
+        )
+
+        assert (
+            output_parameters.scalar(
+                "Equilibrium Climate Sensitivity", "delta_degC"
+            ).value
+            == ecs_magnitude
+        )
+        assert output_parameters.generic("Start Time").value == np.datetime64(
+            "1850-01-01"
+        )
+        assert output_parameters.generic("Stop Time").value == np.datetime64(
+            "2100-01-01"
+        )
