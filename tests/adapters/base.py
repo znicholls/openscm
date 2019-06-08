1658ad3cc68f0de3d9c0ed29481ecad23bcf3803
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2019-06-08 12:10:09 +1000

    Re-write adapter base test class

--- tests/adapters/base.py
+++ tests/adapters/base.py
@@ -1,4 +1,4 @@
-from abc import ABCMeta, abstractmethod
+from abc import ABCMeta, abstractmethod, abstractproperty
 
 import pytest
 
@@ -13,20 +13,29 @@ class _AdapterTester(metaclass=ABCMeta):
     whilst giving authors the ability to tweak the tests as necessary for their specific
     adapter.
     """
+    @abstractproperty
+    def tadapter(self):
+        """
+        Adapter to test
+        """
+        pass
 
-    tadapter = None
-    """
-    Adapter to test
-    """
     @abstractmethod
     def test_initialize(self, test_adapter):
         """
         Test the adapter is initiated as intended.
 
+        Also tests that passed in ``ParameterSet``'s are filled in.
+
         Extra tests should be added for different adapters, to check any other
         expected behaviour as part of ``__init__`` calls.
         """
-        test_adapter._initialize_model()
+        parameters = ParameterSet()
+        output_parameters = ParameterSet()
+        tadapter = self.tadapter(parameters, output_parameters)
+
+        # all models should have a default start time which they set
+        assert not parameters.generic("Start Time").empty
 
     @abstractmethod
     def test_shutdown(self, test_adapter):
@@ -39,33 +48,6 @@ class _AdapterTester(metaclass=ABCMeta):
         del test_adapter
 
     @abstractmethod
-    def test_initialize_model_input(self, test_adapter):
-        """
-        Test that initalizing model input does as intended.
-
-        Extra tests should be added depending on what the adapter should actually
-        do when model input is initialised.
-        """
-        assert not test_adapter._initialized
-        test_adapter.initialize_model_input()
-        assert test_adapter._initialized
-
-    @abstractmethod
-    def test_initialize_run_parameters(self, test_adapter, test_run_parameters):
-        """
-        Test that initalizing run parameters does as intended.
-
-        Extra tests should be added depending on what the adapter should actually
-        do when run parameters are initialised.
-        """
-        self.prepare_run_input(
-            test_adapter, test_run_parameters.start_time, test_run_parameters.stop_time
-        )
-        assert not test_adapter._initialized
-        test_adapter.initialize_run_parameters()
-        assert test_adapter._initialized
-
-    @abstractmethod
     def test_run(self, test_adapter, test_run_parameters):
         """
         Test that running the model does as intended.
@@ -76,8 +58,6 @@ class _AdapterTester(metaclass=ABCMeta):
         self.prepare_run_input(
             test_adapter, test_run_parameters.start_time, test_run_parameters.stop_time
         )
-        test_adapter.initialize_model_input()
-        test_adapter.initialize_run_parameters()
         test_adapter.reset()
         test_adapter.run()
 
@@ -97,8 +77,6 @@ class _AdapterTester(metaclass=ABCMeta):
         self.prepare_run_input(
             test_adapter, test_run_parameters.start_time, test_run_parameters.stop_time
         )
-        test_adapter.initialize_model_input()
-        test_adapter.initialize_run_parameters()
         test_adapter.reset()
         assert test_adapter._current_time == test_run_parameters.start_time
 
@@ -106,7 +84,99 @@ class _AdapterTester(metaclass=ABCMeta):
         assert new_time > test_run_parameters.start_time
 
     @abstractmethod
-    def test_openscm_standard_parameters_handling(self):
+    def test_run_reset_run_same(self, test_adapter, test_run_parameters):
+        """
+        Test that running, resetting and running the model again does as intended.
+
+        Below we give an example of how this could look. Implementers should write their
+        own implementation which tests key output for their model to make this test more
+        robust.
+        """
+        output = test_adapter._output
+
+        self.prepare_run_input(
+            test_adapter, test_run_parameters.start_time, test_run_parameters.stop_time
+        )
+
+        check_args = [
+            "Surface Temperature",
+            "K",
+            np.array(
+                [
+                    np.datetime64("{}-01-01".format(y).astype("datetime64[s]").astype(float))
+                    for y in range(2010, 2090, 10)
+                ]
+            )
+        ]
+        assert output.timeseries(*check_args).empty
+
+        test_adapter.reset()
+        test_adapter.run()
+        first_run_temperature = output.timeseries(*check_args).values
+
+        test_adapter.reset()
+        assert output.timeseries(*check_args).empty
+        test_adapter.run()
+        second_run_temperature = output.timeseries(*check_args).values
+
+        np.testing.assert_allclose(first_run_temperature, second_run_temperature)
+
+    @abstractmethod
+    def test_step_reset_run_same(self, test_adapter, test_run_parameters):
+        """
+        Test that running, resetting, stepping, resetting and running the model again does as intended.
+
+        Below we give an example of how this could look. Implementers should write their
+        own implementation which tests key output for their model to make this test more
+        robust.
+        """
+        output = test_adapter._output
+
+        self.prepare_run_input(
+            test_adapter, test_run_parameters.start_time, test_run_parameters.stop_time
+        )
+
+        check_args = [
+            "Surface Temperature",
+            "K",
+            np.array(
+                [
+                    np.datetime64("{}-01-01".format(y).astype("datetime64[s]").astype(float))
+                    for y in range(2010, 2091, 10)
+                ]
+            )
+        ]
+        assert output.timeseries(*check_args).empty
+
+        test_adapter.reset()
+        test_adapter.run()
+        first_run_temperature = output.timeseries(*check_args).values
+
+        test_adapter.reset()
+        assert output.timeseries(*check_args).empty
+        test_adapter.step()
+        test_adapter.step()
+        first_two_steps_temperature = output.timeseries(
+            "Surface Temperature",
+            "K",
+            np.array(
+                [
+                    np.datetime64("{}-01-01".format(y).astype("datetime64[s]").astype(float))
+                    for y in range(2010, 2031, 10)
+                ]
+            )
+        )
+        np.testing.assert_allclose(first_run_temperature[:2], first_two_steps_temperature)
+
+        test_adapter.reset()
+        assert output.timeseries(*check_args).empty
+        test_adapter.run()
+        second_run_temperature = output.timeseries(*check_args).values
+
+        np.testing.assert_allclose(first_run_temperature, second_run_temperature)
+
+    @abstractmethod
+    def test_openscm_standard_parameters_handling(self, test_adapter):
         """
         Test how the adapter handles OpenSCM's standard parameters.
 
@@ -118,22 +188,19 @@ class _AdapterTester(metaclass=ABCMeta):
 
         We give an example of how such a test might look below.
         """
-        parameters = ParameterSet()
+        parameters = test_adapter._parameters
+        output_parameters = test_adapter._output
+
         parameters.generic("Start Time").value = np.datetime64("1850-01-01")
         parameters.generic("Stop Time").value = np.datetime64("2100-01-01")
         ecs_magnitude = 3.12
         parameters.scalar("Equilibrium Climate Sensitivity", "delta_degC").value = ecs_magnitude
-        output_parameters = ParameterSet()
-
-        test_adapter = self.tadapter(parameters, output_parameters)
 
         self.prepare_run_input(
             test_adapter,
             parameters.generic("Start Time").value,
             parameters.generic("Stop Time").value,
         )
-        test_adapter.initialize_model_input()
-        test_adapter.initialize_run_parameters()
         test_adapter.reset()
         test_adapter.run()
 
@@ -142,10 +209,10 @@ class _AdapterTester(metaclass=ABCMeta):
         # If you're testing the parameters are used as intended, it might look
         # something like:
         assert (
-            test_adapter._parameters.scalar(
+            parameters.scalar(
                 ("Model name", "model ecs parameter"), "delta_degC"
             ).value
-            == ecs_magnitude
+            == ecs_magnitude  # make sure OpenSCM ECS value was used preferentially to the model's ecs
         )
 
         assert (
